b1f97e341b22f2d7b36b1215c7caccabc93dc491
DonggyuJin <jdk2531@gmail.com>
2024-03-13 23:09:03 +0900

    [Main] 초기 프로젝트 세팅

---
+++ hanbat-market/hanbat-market/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hanbat-market
+//
+//  Created by dongs on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hanbat-market/hanbat-market/hanbat_marketApp.swift
@@ -0,0 +1,17 @@
+//
+//  hanbat_marketApp.swift
+//  hanbat-market
+//
+//  Created by dongs on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct hanbat_marketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
